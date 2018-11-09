970205991b4e3c89cf0e74a563a1ff87fbec885f
Energy <energystar.0124@gmail.com>
2018-11-10 02:56:24 +0800

    Add placeholder textures and basic enemy

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float Speed = 1f;
+    public EnemyTarget Target;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Target = FindObjectOfType<EnemyTarget>();
+    }
+
+    void FixedUpdate()
+    {
+//        transform.position += transform.right * Speed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //look at target
+        Vector3 diff = Target.transform.position - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+
+
+        //move towards target
+        transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, Speed * Time.deltaTime);
+    }
+}
