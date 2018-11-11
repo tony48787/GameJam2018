91b31a885e6890fbb0bf139f943b9a84bfaae4da
Energy <energystar.0124@gmail.com>
2018-11-12 01:57:24 +0800

    Some refactoring to enemy, add health and attack range

---
+++ Assets/Scripts/Common/HealthComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+public class HealthComponent : MonoBehaviour
+{
+    public float CurrentHeath;
+    public float InitialHealth;
+    public float MaxHealth;
+    
+    // Use this for initialization
+    void Start()
+    {
+        CurrentHeath = InitialHealth;
+        
+    }
+}
---
+++ Assets/Scripts/Enemy/BasicEnemyController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour
+{
+    public float Speed = 1f;
+    public GameObject Target;
+    public float AttackRange = 10f;
+    public HealthComponent Health;
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Target = FindObjectOfType<EnemyTarget>().gameObject;
+        Health = GetComponent<HealthComponent>();
+    }
+
+    void FixedUpdate()
+    {
+//        transform.position += transform.right * Speed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoLookAt(Target);
+
+        float dist = Vector3.Distance(Target.transform.position, transform.position);
+
+        if (dist <= AttackRange)
+        {
+            DoAttack(Target);
+        }
+        else
+        {
+            DoMove(Target);
+        }
+    }
+
+    public virtual void DoMove(GameObject enemyTarget)
+    {
+//move towards target
+        transform.position =
+            Vector2.MoveTowards(transform.position, Target.transform.position, Speed * Time.deltaTime);
+    }
+
+    public virtual void DoLookAt(GameObject enemyTarget)
+    {
+        //look at target
+        Vector3 diff = enemyTarget.transform.position - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
+
+    public virtual void DoAttack(GameObject target)
+    {
+    }
+}
