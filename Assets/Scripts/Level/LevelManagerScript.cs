a5a52089685e341dd8b9f9b87dce32e4aa63dbf9
tony48787 <tony65548711tony@yahoo.com.hk>
2018-11-12 00:37:23 +0800

    can place tower in a map now,

---
+++ Assets/Scripts/Level/LevelManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject tile;
+
+    [SerializeField]
+    private GameObject tower;
+
+    // Use this for initialization
+    void Start ()
+    {
+        CreateLevel();
+    }
+
+    private void CreateLevel()
+    {
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        int rowMax = 15;
+        int colMax = 38;
+        for (int row = 0; row < rowMax; row++)
+        {
+            for (int col = 0; col < colMax; col++)
+            {
+                PlaceTile(row, col, worldStart);
+            }
+        }
+    }
+
+    private void PlaceTile(int row, int col, Vector3 worldStart)
+    {
+        float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+
+        GameObject newTile = Instantiate(tile);
+
+        newTile.transform.position = new Vector3(worldStart.x + (tileSize * col), worldStart.y - (tileSize * row), 0);
+
+        newTile.GetComponent<TileScript>().Init(new Point(col, row), 1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public GameObject getTower()
+    {
+        return tower;
+    }
+}
