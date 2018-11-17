7c594e16d2e220f221cbc776bf99c51b40f56ae3
tony48787 <tony65548711tony@yahoo.com.hk>
2018-11-17 20:44:40 +0800

    add game manager, wave logic

---
+++ Assets/Scripts/Level/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject enemy;
+
+    [SerializeField]
+    private int spawnCount = 10;
+
+    private int currentCount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void StartSpawn()
+    {
+        currentCount = spawnCount;
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            GameObject newEnemy = Instantiate(enemy);
+        }
+    }
+
+    void EndSpawn()
+    {
+        gameObject.SendMessage("EndWave");
+    }
+
+    public void UpdateCurrentCountBy(int delta = 1)
+    {
+        currentCount -= delta;
+        if (currentCount <= 0)
+        {
+            EndSpawn();
+        }
+    }
+}
