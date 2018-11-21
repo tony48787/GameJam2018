c59e95b9d4b8bd1b9599cb971c2abec084f3fd76
ACMYOKS <acmyoks1412@gmail.com>
2018-11-22 01:50:01 +0800

    Add health pack and charge bar prefab

---
+++ Assets/Scripts/Common/ChargeBar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeBar : MonoBehaviour {
+
+	private Transform bar;
+	private Transform barSprite;
+	private SpriteRenderer barSpriteRenderer;
+	private Transform border;
+	private Transform background;
+	public float width = 100f;
+	public float height = 10f;
+	public float borderSize = 2f;
+	private float chargeRatio = 1f;
+	private float currentChargeRatio;
+	public float lerpRate = 1f;
+	
+	// Use this for initialization
+	void Start () {
+		bar = transform.Find("Bar");
+		barSprite = bar.Find("BarSprite");
+		barSpriteRenderer = barSprite.GetComponent<SpriteRenderer>();
+		border = transform.Find("Border");
+		background = transform.Find("Background");
+		border.localScale = new Vector3(width+borderSize*2, height+borderSize*2);
+		barSprite.localScale = new Vector3(width, height);
+		background.localScale = new Vector3(width, height);
+		bar.localPosition = new Vector3(bar.localPosition.x*width/100f, 0);
+		barSprite.localPosition = new Vector3(barSprite.localPosition.x*width/100f, 0);
+
+		currentChargeRatio = chargeRatio;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs(currentChargeRatio - chargeRatio) > 0.01) {
+			currentChargeRatio = Mathf.Lerp(currentChargeRatio, chargeRatio, lerpRate);
+			if (currentChargeRatio < 0) {
+				currentChargeRatio = 0;
+			}
+			bar.localScale = new Vector3(currentChargeRatio, 1f);
+			ChangeBarColor();
+		}
+	}
+
+	void ChangeBarColor() {
+		Color tmp = barSpriteRenderer.color;
+		if (currentChargeRatio == 1f) {
+			tmp.g = 1f;
+		}
+		else {
+			tmp.g = 0.6f;
+		}
+		barSpriteRenderer.color = tmp;
+	}
+
+	public void SetChargeRatio(float chargeRatio) {
+		this.chargeRatio = chargeRatio;
+	}
+}
---
+++ Assets/Scripts/Item/HealthPack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour {
+
+	public float recoverHp = 20f;
+	private CircleCollider2D circleCollider2D;
+	// Use this for initialization
+	void Start () {
+		circleCollider2D = GetComponent<CircleCollider2D>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!other.isTrigger && other.CompareTag("Player")) {
+			HeroController player = other.GetComponent<HeroController>();
+			player.hp += recoverHp;
+			if (player.hp >= player.maxHp) {
+				player.hp = player.maxHp;
+			}
+			Destroy(gameObject);
+		}
+	}
+}
