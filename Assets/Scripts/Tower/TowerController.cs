177f0da6d9dfe885752cdd123e84d32bd852ff7b
tony48787 <tony65548711tony@yahoo.com.hk>
2018-11-27 17:26:59 +0800

    Update tower behaviour to shoot closest enemy it can find

--- Assets/Scripts/Tower/TowerController.cs
+++ Assets/Scripts/Tower/TowerController.cs
@@ -50,9 +50,14 @@ public class TowerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        enemy = GameObject.FindGameObjectWithTag("Enemy");
-        if (EnemyInRange() && !overheat) {
-            ShootEnemy();
+        if (!overheat) {
+            if (EnemyInRange())
+            {
+                ShootEnemy();
+            } else
+            {
+                enemy = FindClosestEnemy();
+            }
         } else
         {
             Cooldown();
@@ -60,6 +65,26 @@ public class TowerController : MonoBehaviour {
 
         
 	}
+
+    GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
 
     bool EnemyInRange()
     {
