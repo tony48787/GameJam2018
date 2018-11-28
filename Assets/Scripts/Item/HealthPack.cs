75991fcba8aa0a7f180e19fe2814c328ff79f1a8
tony48787 <tony65548711tony@yahoo.com.hk>
2018-11-29 00:12:48 +0800

    5 percent of chance to drop a health pack when an enemy is defeated

--- Assets/Scripts/Item/HealthPack.cs
+++ Assets/Scripts/Item/HealthPack.cs
@@ -6,18 +6,22 @@ public class HealthPack : MonoBehaviour {
 
 	public float recoverHp = 20f;
 	private CircleCollider2D circleCollider2D;
+    private GameManager gm;
+    private PlayerStatus playerStatus;
 	// Use this for initialization
 	void Start () {
 		circleCollider2D = GetComponent<CircleCollider2D>();
-	}
+        gm = GameManager.instance;
+        playerStatus = gm.playerStatus;
+    }
 	
 	void OnTriggerEnter2D(Collider2D other) {
 		if (!other.isTrigger && other.CompareTag("Player")) {
-			HeroController player = other.GetComponent<HeroController>();
-			player.hp += recoverHp;
-			if (player.hp >= player.maxHp) {
-				player.hp = player.maxHp;
-			}
+            playerStatus.currentHp += recoverHp;
+			if (playerStatus.currentHp >= playerStatus.maxHp) {
+                playerStatus.currentHp = playerStatus.maxHp;
+            }
+            Debug.Log("Current: " + playerStatus.currentHp);
 			Destroy(gameObject);
 		}
 	}
